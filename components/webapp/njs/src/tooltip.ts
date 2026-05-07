d56e5b5ab9dda362354a86c69beb716b58773cf0
zwiebert <bertw@online.de>
2026-05-07 14:56:05 +0200

    Update njs packages

--- components/webapp/njs/src/tooltip.ts
+++ components/webapp/njs/src/tooltip.ts
@@ -1,5 +1,10 @@
 import tippy, { type Props } from 'tippy.js';
+import 'tippy.js/dist/tippy.css';
+import 'tippy.js/themes/light.css';
 import type { Action } from 'svelte/action';
+
+// Set global defaults for the entire app
+tippy.setDefaultProps({ theme: 'light' });
 
 // Use Partial<Props> because you don't need to provide every single Tippy option
 export const tooltip: Action<HTMLElement, Partial<Props>> = (node, options) => {
